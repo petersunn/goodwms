1bf71307fe9d60c898f36177fe070a18cb5743b8
Peter Sun <Peter.Sun@t2vsoft.com>
2014-05-05 18:14:09 +0800

    Remove database project

--- wms-data/Roles.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Roles]
-(
-	[RoleId] INT IDENTITY (1, 1) NOT NULL,
-	RoleName NVARCHAR(50) NULL, 
-	[CreatedBy] int null,
-	[CreatedOn] datetime null,
-	[UpdatedBy] int null,
-	[UpdatedOn] datetime null,
-)
--- wms-data/Whse.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Whse]
-(
-	[WhId] INT IDENTITY (1, 1) NOT NULL,
-	[WhName] NVARCHAR(50) NULL,
-	[CreatedBy] int null,
-	[CreatedOn] datetime null,
-	[UpdatedBy] int null,
-	[UpdatedOn] datetime null,
-)
